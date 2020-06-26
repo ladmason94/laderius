ba1b4ea49fce9cda6b305ba111c9b241d639c282
ladmason94 <62527989+ladmason94@users.noreply.github.com>
2020-06-26 12:54:28 -0400

    Add files via upload

---
+++ movements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movements : MonoBehaviour
+{
+    private object tf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform transform1 = GetComponent<Transform>();
+          
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+      Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.1f, 1.0f);
+      transform.position = transform.position + horizontal * Time.deltaTime; 
+    
+          
+    }
+}
